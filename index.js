4b198009cd2d184fa09b3486207dce2c9efcdf76
next75 <developer.help.905@gmail.com>
2024-05-28 09:22:31 +0530

    number is palindrome or not

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+
+// return true if given number is palindrome
+
+const palindromeNumber = num => {
+    let originalNum = num;
+    let newNum = 0;
+    while (num > 0) {
+        let digit = num % 10;
+        newNum = newNum * 10 + digit;
+        num = Math.floor(num / 10);
+    }
+    return newNum === originalNum;
+};
+console.log(palindrome(12121));
