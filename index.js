2218dca9cc70f64010ff5a2f93bc4017631c3b88
next75 <developer.help.905@gmail.com>
2024-05-30 08:25:00 +0530

    find LCM of three numbers

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-// fimd factorial of given number
+// find least comman multiple of three numbers
 
-const getFactorial = num => {
-    let result = 1;
-    if (num <= 1) {
-        return result;
+const findLCM = (first, second, third) => {
+  let i = 1
+    while (true ) {
+        let factor = first * i;
+        if (factor % second === 0 && factor % third === 0) {
+            return factor;
+        }
+        i++
     }
-    for (let i = 2; i <= num; i++) {
-        result *= i;
-    }
-    return result;
 };
 
-console.log(getFactorial(3));
+console.log(findLCM(4, 6, 8)); // 24
+console.log(findLCM(4, 5, 6)); // 60
+console.log(findLCM(3, 7, 14)); // 42
+console.log(findLCM(2, 3, 4)); // 12
