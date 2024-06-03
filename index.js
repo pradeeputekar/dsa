1567207c5b3b14da27034f6a8624dfb88f62d15e
next75 <developer.help.905@gmail.com>
2024-06-03 20:34:04 +0530

    check string has unique value

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const primeNumber = num => {
-    if (num <= 1) {
-        return false;
-    }
-    if (num === 2) {
-        return true;
-    }
-    if (num % 2 === 0) {
-        return false; // any even number greater than 2 is not prime
-    }
-    for (let i = 3; i <= Math.sqrt(num); i += 2) {
-        if (num % i === 0) {
-            return false;
+function hasUniqueCharacters(str) {
+    let charSet = new Set();
+    for (let char of str) { 
+        if (charSet.has(char)) {
+            return false;  
         }
+        charSet.add(char);
     }
+    
     return true;
-};
+}
 
-console.log(primeNumber(3)); // true
+console.log(hasUniqueCharacters("abcdef")); 
+console.log(hasUniqueCharacters("hello"));  
