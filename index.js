65edb2edbaf2dc1762349cc534231e05e4132b35
next75 <developer.help.905@gmail.com>
2024-06-03 18:16:53 +0530

    prime numbers

--- index.js
+++ index.js
@@ -1,38 +1,19 @@
-// sort given array
-
-const sortArray = arr => {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
-                let temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+const primeNumber = num => {
+    if (num <= 1) {
+        return false;
+    }
+    if (num === 2) {
+        return true;
+    }
+    if (num % 2 === 0) {
+        return false; // any even number greater than 2 is not prime
+    }
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+        if (num % i === 0) {
+            return false;
         }
     }
-    return arr;
-};
-console.log(sortArray([1, 3, 4, 2]));
-
-// OR
-
-const quicksort = arr => {
-    if (arr.length <= 1) {
-        return arr;
-    }
-    let pivot = arr[0];
-    let left = [];
-    let right = [];
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
-        }
-    }
-
-    return quicksort(left).concat(pivot, quicksort(right));
+    return true;
 };
 
-console.log(quicksort([1, 3, 4, 2]));
+console.log(primeNumber(3)); // true
